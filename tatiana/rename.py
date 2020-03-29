f83a74b222fc7b9d4bc1ca43110dc849c1c5da27
oliverwk <30601311+oliverwk@users.noreply.github.com>
2020-03-29 19:46:37 +0200

    sa

---
+++ tatiana/rename.py
@@ -0,0 +1,24 @@
+import os
+count = 0
+while (count < 20):
+    count = count + 1
+
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(1\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_1.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(2\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_2.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(3\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_3.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(4\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_4.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(5\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_5.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(6\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_6.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(7\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_7.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(8\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_8.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(9\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_9.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(10\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_10.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(11\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_11.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(12\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_12.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(13\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_13.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(14\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_14.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(15\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_15.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(16\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_16.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(17\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_17.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(18\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_18.jpg')
+    os.rename(r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/70932798_1072836726441605_527163624535307822_n\ \(19\).jpg',r'/Users/MWK/Documents/GitHub/site/oliverwk.github.io/tatiana/tatiana_art_19.jpg')
