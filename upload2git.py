2abff98b0ef740b492540778dc61896f27dc2317
øllen <30601311+oliverwk@users.noreply.github.com>
2021-01-20 21:26:28 +0100

    Update upload2git.py

--- upload2git.py
+++ upload2git.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python3
 import requests, sys, datetime, base64, json
-file = open("./luchtdruk.html","r")
+file = open("luchtdruk.html","r")
 rHtml = file.read()
 file.close()
 file = "luchtdruk.html"
 content_res = requests.get("https://api.github.com/repos/oliverwk/oliverwk.github.io/contents/"+file)
-data = {"message": str("updated {} from Github Actions on {} using the RAW Github api ".format(file, datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"))) , "content": base64.b64encode(rHtml.encode('ascii')).decode('utf-8'), "sha": str(json.loads(content_res.text)["sha"])}
-update_res = requests.put("https://api.github.com/repos/oliverwk/oliverwk.github.io/contents/"+file, data=json.dumps(data, indent=4), auth=('oliverwk', str(sys.argv[1])))
+data = {"message": str("updated {} from Github Actions on {} using the RAW Github api ".format(file, datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"))) , "content": base64.b64encode(rHtml.replace("leaflet", "Luchtdruk", 1).encode('ascii')).decode('utf-8'), "sha": str(json.loads(content_res.text)["sha"])}
+update_res = requests.put("https://api.github.com/repos/oliverwk/oliverwk.github.io/contents/"+file, data=json.dumps(data, indent=4), auth=('oliverwk',  str(sys.argv[1])))
 print(update_res.status_code)
 print(update_res.json())
 print("Pushed to Github" if update_res.status_code == 200 else "Didn't pushed to Github" )
+sys.exit(0 if update_res.status_code == 200 else 1)
