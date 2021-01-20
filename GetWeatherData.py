f1e475e0cb7437db9a2da2a79a3e8a77702a6b9b
øllen <30601311+oliverwk@users.noreply.github.com>
2021-01-20 21:26:55 +0100

    Update GetWeatherData.py

--- GetWeatherData.py
+++ GetWeatherData.py
@@ -7,7 +7,7 @@ page_xml_feed = requests.get(url_xml_feed)
 soup = BeautifulSoup(page_xml_feed.content, "xml")
 luchtdruk = []
 for station in soup.find_all('weerstation'):
-    ll = float(station.luchtdruk.get_text()) - 1010.0 if station.luchtdruk.get_text() != "-" else 0.0
+    ll = float(station.luchtdruk.get_text()) if station.luchtdruk.get_text() != "-" else 0.0
     luchtdruk.append({"luchtdruk":ll,"lat":station.lat.get_text(),"lon":station.lon.get_text(),"windrichting":station.windrichting.get_text()})
 
 
@@ -18,6 +18,6 @@ for i in jsons:
     csv += str(str(i["luchtdruk"])+","+i["lat"]+","+i["lon"]+"\n")
 
 print(csv)
-file = open("./weather.csv","w")
+file = open("weather.csv","w")
 file.writelines(csv)
 file.close()
