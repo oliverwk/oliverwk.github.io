b32ce3ac261c21cc28d9b0a22e65532874211ab1
øllen <30601311+oliverwk@users.noreply.github.com>
2021-01-21 09:09:52 +0100

    Update GetWeatherData.py

--- GetWeatherData.py
+++ GetWeatherData.py
@@ -4,7 +4,7 @@ import requests, json
 from bs4 import BeautifulSoup
 url_xml_feed = "https://data.buienradar.nl/1.0/feed/xml"
 page_xml_feed = requests.get(url_xml_feed)
-soup = BeautifulSoup(page_xml_feed.content, "xml")
+soup = BeautifulSoup(page_xml_feed.content, "lxml")
 luchtdruk = []
 for station in soup.find_all('weerstation'):
     ll = float(station.luchtdruk.get_text()) if station.luchtdruk.get_text() != "-" else 0.0
